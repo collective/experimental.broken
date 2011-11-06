4eb0870c5c11d7fec5e213ed8c8811bde264266d
Ross Patterson <me@rpatterson.net>
2011-11-05 17:56:58 -0700

    package structure

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+tests_require = ['zope.testing']
+
+setup(name='experimental.brokeninterfaces',
+      version='0.1',
+      description="Better ZODB handling of broken interfaces",
+      long_description=
+      open(os.path.join(
+          "experimental", "brokeninterfaces", "README.txt")).read() + '\n\n' +
+      open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Ross Patterson',
+      author_email='me@rpatterson.net',
+      url='http://pypi.python.org/pypi/rpatterson.listfile',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['experimental'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'ZODB3',
+          'zope.interface',
+      ],
+      test_suite = "experimental.brokeninterfaces.tests.test_suite",
+      tests_require=tests_require,
+      extras_require=dict(test=tests_require),
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
