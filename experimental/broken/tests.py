1795a2b26a869263ab8d70b81c720bccdd6e2a18
Ross Patterson <me@rpatterson.net>
2011-11-06 22:15:25 -0800

    Test coverage for persistent component registry failing at the right places.

--- experimental/broken/tests.py
+++ experimental/broken/tests.py
@@ -10,7 +10,18 @@ from ZODB import broken
 class IFoo(interface.Interface): pass
 
 
-class Foo(persistent.Persistent): pass
+class IBar(interface.Interface): pass
+
+
+class IQux(interface.Interface): pass
+
+
+class Bar(object):
+
+    def __call__(self, *args, **kw): pass
+
+
+class Foo(Bar, persistent.Persistent): pass
 
 
 def reset():
@@ -18,9 +29,25 @@ def reset():
     broken.broken_cache.clear()
 
 
+def tearDown(self=None, orig_ProvidesClass=declarations.ProvidesClass,
+             orig_IFoo=IFoo, orig_IQux=IQux, orig_Foo=Foo, orig_Bar=Bar):
+    reset()
+    declarations.ProvidesClass = orig_ProvidesClass
+    global IFoo
+    IFoo = orig_IFoo
+    global IQux
+    IQux = orig_IQux
+    global Foo
+    Foo = orig_Foo
+    global Bar
+    Bar = orig_Bar
+
+
 def test_suite():
     return doctest.DocFileSuite(
         'interface.txt',
+        'registry.txt',
+        tearDown=tearDown,
         optionflags=(
             doctest.ELLIPSIS|
             doctest.NORMALIZE_WHITESPACE|
